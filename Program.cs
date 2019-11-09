214e81e67202c8341abad00fb3a0506a38a92350
Sindre S. Kjær <sindre@sindrekjr.no>
2019-11-09 21:03:32 +0100

    Removed "Base" from namespace and project name

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
-﻿using System;
+using System;
 
-namespace AdventOfCodeBase {
+namespace AdventOfCode {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
