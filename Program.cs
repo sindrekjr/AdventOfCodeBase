4127d43a41dfc7c2a6fce9f0775dbd8cff3543ea
Sindre S. Kjær <sindre@sindrekjr.no>
2019-11-19 21:08:49 +0100

    Essentials of Program, Config, and ASolution: * Program handles retrieval of all solution classes * Config handles deserializes config.json, as well as validation of those values * ASolution handles lazy retrieval of input, lazy solution calls, and output

--- Program.cs
+++ Program.cs
@@ -1,9 +1,89 @@
+﻿using AdventOfCode.Solutions;
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text.Json;
+using System.Text.RegularExpressions;
 
 namespace AdventOfCode {
+
     class Program {
+
+        public static readonly Config config = JsonSerializer.Deserialize<Config>(File.ReadAllText("config.json").ToLower());
+        static IEnumerable<ASolution> solutions = GetAllSolutions();
+
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            if(string.IsNullOrWhiteSpace(config.cookie)) return;
+
+            if(config.day == 0) {
+                foreach(ASolution s in solutions) {
+                    Console.WriteLine(s.Solve()); 
+                }
+            } else {
+                Console.WriteLine(GetSolution(config.day).Solve());
+            }
+        }
+
+        private static ASolution GetSolution(int day) {
+            return solutions.Single(x => x.Day == day);
+        }
+        private static IEnumerable<ASolution> GetAllSolutions() {
+            foreach(Type type in Assembly.GetExecutingAssembly().GetTypes().Where(type => type.Namespace == $"AdventOfCode.Solutions.Year{config.year}")) {
+                yield return (ASolution) Activator.CreateInstance(type);
+            }
+        }
+    }
+
+    internal class Config {
+        private string _c; 
+        private int _y;
+        private int _d;
+        private int _p; 
+
+        public string cookie { 
+            get => _c;
+            set {
+                if(Regex.IsMatch(value, "^session=[a-z0-9]+$")) {
+                    _c = value; 
+                } else {
+                    _c = null;  
+                }
+            }
+        }
+        public int year { 
+            get => _y;
+            set {
+                if(value < 2015 || value > DateTime.Now.Year) {
+                    _y = DateTime.Now.Year; 
+                    Console.WriteLine("Invalid year supplied; set to current year {} by default.", _y);
+                } else {
+                    _y = value; 
+                }
+            } 
+        }
+        public int day { 
+            get => _d;
+            set {
+                if(value < 0 || value > 25) {
+                    _d = (DateTime.Now.Month == 12) ? DateTime.Now.Day : 0; 
+                    Console.WriteLine("Invalid day supplied; set to {} by default", _d);
+                } else {
+                    _d = value; 
+                }
+            }
+        }
+        public int part { 
+            get => _p;
+            set {
+                if(value < 0 || value > 2) {
+                    _p = 0; 
+                    Console.WriteLine("Invalid part value supplied; set to {} by default.", _p); 
+                } else {
+                    _p = value; 
+                }
+            }
         }
     }
 }
--- Solutions/ASolution.cs
+++ Solutions/ASolution.cs
@@ -1,24 +1,71 @@
+using System;
+using System.IO; 
+using System.Net;
+
 namespace AdventOfCode.Solutions {
 
     abstract class ASolution {
+        
+        // Public fields
+        public int Day;
+        public int Year; 
+        public string Title;
 
-        public int day;
-        public string title;
-        public string input;
+        // Private lazy fields accessed by public properties
+        private Lazy<string> _input, _part1, _part2;
 
-        protected ASolution(int day, string title) {
-            this.day = day;
-            this.title = title;
+        // Public properties 
+        public string Input => _input.Value; 
+        public string Part1 => "Part 1: " + (string.IsNullOrEmpty(_part1.Value) ? "Unsolved" : _part1.Value);
+        public string Part2 => "Part 2: " + (string.IsNullOrEmpty(_part2.Value) ? "Unsolved" : _part2.Value);
+
+        // Constructor
+        private protected ASolution(int day, int year, string title) {
+            Day = day;
+            Year = year; 
+            Title = title;
+            _input = new Lazy<string>(LoadInput());
+            _part1 = new Lazy<string>(SolvePartOne());
+            _part2 = new Lazy<string>(SolvePartTwo());
         }
 
-        protected abstract string PartOne();
-        protected abstract string PartTwo();
+        public string Solve(int part = 0) {
+            string output = $"--- Day {Day}: {Title} --- \n";
+            if(part == 0 || part == 1) {
+                output += $"{Part1}\n"; 
+            }
+            if(part == 0 || part == 2) {
+                output += $"{Part2}\n";
+            }
+            return output; 
+        }
 
-        public int GetDay() => this.day;
-        public string GetTitle() => this.title;
-        protected string GetInput() => this.input;
+        // Method for retrieving the puzzle input for the given day
+        private string LoadInput() {
+            string INPUT_FILEPATH = $"./Solutions/Year{Year}/Day{Day.ToString("D2")}/input";
+            string INPUT_URL = $"https://adventofcode.com/{Year}/day/{Day}/input";
 
+            if(!File.Exists(INPUT_FILEPATH)) {
+                try {
+                    using (WebClient client = new WebClient()) {
+                        client.Headers.Add(HttpRequestHeader.Cookie, Program.config.cookie);
+                        File.WriteAllText(INPUT_FILEPATH, client.DownloadString(INPUT_URL).Trim());
+                    }
+                } catch (WebException e) {
+                    if (e.Status.Equals(400)) {
+                        Console.WriteLine("Error code 400 when requesting input through web client. Make sure your cookie is configured correctly.");
+                    } else if (e.Status.Equals(404)) {
+                        Console.WriteLine("Error code 404 when requesting input through web client. It might not be available yet.");
+                    } else {
+                        Console.WriteLine(e.ToString());
+                    }
+                }
+            }
+            return File.ReadAllText(INPUT_FILEPATH);
+        }
+
+        // Abstract methods that must be defined in each instantiated subclass of ASolution
+        protected abstract string SolvePartOne();
+        protected abstract string SolvePartTwo();
     }
-
-    enum Part { All, One, Two }
 }
