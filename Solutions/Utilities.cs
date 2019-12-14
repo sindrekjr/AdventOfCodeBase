11c3025215ee7cf567dcb62121a397c2f859eef1
Sindre S. Kjær <sindre@sindrekjr.no>
2019-12-14 13:52:49 +0100

    Utility.Deconstruct()

--- Solutions/Utilities.cs
+++ Solutions/Utilities.cs
@@ -92,5 +92,17 @@ namespace AdventOfCode.Solutions {
                 yield return array.Skip(i * size).Take(size);
             }
         }
+
+        // https://stackoverflow.com/questions/49190830/is-it-possible-for-string-split-to-return-tuple
+        public static void Deconstruct<T>(this IList<T> list, out T first, out IList<T> rest) {
+            first = list.Count > 0 ? list[0] : default(T); // or throw
+            rest = list.Skip(1).ToList();
+        }
+
+        public static void Deconstruct<T>(this IList<T> list, out T first, out T second, out IList<T> rest) {
+            first = list.Count > 0 ? list[0] : default(T); // or throw
+            second = list.Count > 1 ? list[1] : default(T); // or throw
+            rest = list.Skip(2).ToList();
+        }
     }
 }
