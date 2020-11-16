93b3e84879e2daccc4fb3f54910b379e47b9dece
Bpendragon (David Camp) <david@campclan.net>
2020-11-15 22:54:42 -0800

    Added Solution file generator script, along with usage notes in the README

---
+++ AdventOfCode/GenerateSolutionFiles.ps1
@@ -0,0 +1,44 @@
+﻿param(
+    [int]$Year = (Get-Date).Year
+)
+
+$year = 2015
+$template = @"
+namespace AdventOfCode.Solutions.Year<YEAR>
+{
+
+    class Day<DAY> : ASolution
+    {
+
+        public Day<DAY>() : base(<DAY>, <YEAR>, `"`")
+        {
+
+        }
+
+        protected override string SolvePartOne()
+        {
+            return null;
+        }
+
+        protected override string SolvePartTwo()
+        {
+            return null;
+        }
+    }
+}
+"@
+
+$newDirectory = Join-Path $PSScriptRoot "Solutions" "Year$Year" 
+
+if(!(Test-Path $NewDirectory)){
+    New-Item $newDirectory -ItemType Directory | Out-Null
+}
+
+for($i = 1; $i -le 25; $i++) {
+    $newFile = Join-Path $newDirectory "Day$("{0:00}" -f $i)"  "Solution.cs"
+    if(!(Test-Path $newFile)){
+        New-Item $newFile -ItemType File -Value ($template -replace "<YEAR>", $Year -replace "<DAY>", "$("{0:00}" -f $i)") -Force | Out-Null
+    }
+}
+
+Write-Host "Files Generated"
