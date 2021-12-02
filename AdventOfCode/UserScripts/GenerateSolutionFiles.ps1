2bb788a135b02704bd84765ec9ba4d8c5223866b
ElrohirGT <elrohirgt@gmail.com>
2021-12-02 14:55:00 -0600

    Changed the powershell script to use Path class

--- AdventOfCode/UserScripts/GenerateSolutionFiles.ps1
+++ AdventOfCode/UserScripts/GenerateSolutionFiles.ps1
@@ -32,14 +32,14 @@ namespace AdventOfCode.Solutions.Year<YEAR>
 
 "@
 
-$newDirectory = Join-Path $PSScriptRoot ".." "Solutions" "Year$Year" 
+$newDirectory = [IO.Path]::Combine($PSScriptRoot, "..", "Solutions", "Year$Year")
 
 if(!(Test-Path $newDirectory)) {
     New-Item $newDirectory -ItemType Directory | Out-Null
 }
 
 for($i = 1; $i -le 25; $i++) {
-    $newFile = Join-Path $newDirectory "Day$("{0:00}" -f $i)"  "Day$("{0:00}" -f $i).cs"
+    $newFile = [IO.Path]::Combine($newDirectory, "Day$("{0:00}" -f $i)", "Day$("{0:00}" -f $i).cs")
     if(!(Test-Path $newFile)) {
         New-Item $newFile -ItemType File -Value ($template -replace "<YEAR>", $Year -replace "<DAY>", "$("{0:00}" -f $i)") -Force | Out-Null
     }
