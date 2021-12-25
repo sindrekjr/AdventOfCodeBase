6ab34622ba8f624ca6d3a72447dd1f9e5c217e25
Sindre S. Kjær <sindre@sindrekjr.no>
2021-12-25 19:07:35 +0100

    refactor: moved LoadInput to SolutionBase

--- AdventOfCode.Solutions/SolutionBase.cs
+++ AdventOfCode.Solutions/SolutionBase.cs
@@ -5,7 +5,7 @@ global using System.Linq;
 global using System.Text;
 
 using System.Diagnostics;
-
+using System.Net;
 using AdventOfCode.Services;
 
 namespace AdventOfCode.Solutions
@@ -16,8 +16,8 @@ namespace AdventOfCode.Solutions
         public int Year { get; }
         public string Title { get; }
         public bool Debug { get; set; }
-        public string? Input => InputService.FetchInput(Day, Year);
-        public string? DebugInput => InputService.FetchDebugInput(Day, Year);
+        public string? Input => LoadInput().Result;
+        public string? DebugInput => LoadDebugInput();
 
         public SolutionResult Part1 => SolveSafely(SolvePartOne);
         public SolutionResult Part2 => SolveSafely(SolvePartTwo);
@@ -77,6 +77,75 @@ namespace AdventOfCode.Solutions
                     throw;
                 }
             }
+        }
+
+        static HttpClientHandler handler = new HttpClientHandler { UseCookies = true };
+        static HttpClient client = new HttpClient(handler);
+
+        async Task<string> LoadInput()
+        {
+            var inputFilepath = $"./AdventOfCode.Solutions/Year{Year}/Day{Day:D2}/input";
+            var inputUri = new Uri($"https://adventofcode.com/{Year}/day/{Day}/input");
+
+            if (File.Exists(inputFilepath) && new FileInfo(inputFilepath).Length > 0)
+            {
+                return File.ReadAllText(inputFilepath);
+            }
+
+            try
+            {
+                var currentEst = TimeZoneInfo.ConvertTime(DateTime.Now, TimeZoneInfo.Utc).AddHours(-5);
+                if (currentEst < new DateTime(Year, 12, Day)) throw new InvalidOperationException("Too early.");
+
+                handler.CookieContainer.Add(new Cookie { Name = "session", Domain = ".adventofcode.com", Value = ConfigService.GetCookie() });
+                var request = new HttpRequestMessage { RequestUri = inputUri, Method = HttpMethod.Get };
+
+                var response = await client.SendAsync(request);
+                response.EnsureSuccessStatusCode();
+
+                var input = await response.Content.ReadAsStringAsync();
+                File.WriteAllText(inputFilepath, input);
+
+                return input;
+            }
+            catch (HttpRequestException e)
+            {
+                var code = e.StatusCode;
+                var colour = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                if (code == HttpStatusCode.BadRequest)
+                {
+                    Console.WriteLine($"Day {Day}: Received 400 when attempting to retrieve puzzle input. Your session cookie is probably not recognized.");
+
+                }
+                else if (code == HttpStatusCode.NotFound)
+                {
+                    Console.WriteLine($"Day {Day}: Received 404 when attempting to retrieve puzzle input. The puzzle is probably not available yet.");
+                }
+                else
+                {
+                    Console.ForegroundColor = colour;
+                    Console.WriteLine(e.ToString());
+                }
+                Console.ForegroundColor = colour;
+            }
+            catch (InvalidOperationException)
+            {
+                var colour = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.WriteLine($"Day {Day}: Cannot fetch puzzle input before given date (Eastern Standard Time).");
+                Console.ForegroundColor = colour;
+            }
+
+            return "";
+        }
+
+        string LoadDebugInput()
+        {
+            string inputFilepath = $"./AdventOfCode.Solutions/Year{Year}/Day{Day:D2}/debug";
+            return (File.Exists(inputFilepath) && new FileInfo(inputFilepath).Length > 0)
+                ? File.ReadAllText(inputFilepath)
+                : "";
         }
 
         public override string ToString() =>
